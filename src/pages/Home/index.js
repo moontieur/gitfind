daf866a50125ec8b2de707f65eec772e48b96bcf
user.name <caiohmq12@gmail.com>
2023-08-09 17:49:42 -0300

    FullApp

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,26 +3,25 @@ import Header from '../../components/Header';
 import background from '../../assets/background3.svg';
 import List from '../../components/List';
 import './styles.css';
-
-function App() {
-  const [user, setUser] = useState('');
+ 
+function App () {
+  const [user, setUser] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
   const [repos, setRepos] = useState(null);
 
   const handleGetData = async () => {
-    const userData = await 
-    fetch(`https://api.github.com/users/${user}`);
+    const userData = await fetch(`https://api.github.com/users/${user}`);
     const newUser = await userData.json();
+    console.log(newUser);
 
     if (newUser.name) {
-      const { avatar_url, name, bio } = newUser;
-      setCurrentUser({ avatar_url, name, bio });
+      const {avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
 
-      const reposData = await fetch
-      (`https://api.github.com/users/${user}/repos`);
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
       const newRepos = await reposData.json();
-      
-      if(newRepos.length) {
+
+      if (newRepos.length) {
         setRepos(newRepos);
       }
     }
@@ -32,46 +31,47 @@ function App() {
     <div className="App">
       <Header />
       <div className="conteudo">
-        <img src={background} className="background" alt="background app" />
-        <div className='info'>
+        <img src={background}  className="background" alt="background app" />
+        <div className="info">
           <div>
             <input 
               name="usuario"
               value={user}
               onChange={(event) => setUser(event.target.value)}
-              placeholder='@username' 
+              placeholder="@username"
             />
-              <button onClick={handleGetData}>Buscar</button>
+            <button onClick={handleGetData}>Buscar</button>
           </div>
-          {currentUser?.name ? ( 
+          {currentUser?.name ? (
             <>
-              <div className='perfil'>
+              <div className="perfil">
                 <img
-                 src={currentUser.avatar_url}
-                 className='profile'
-                 alt="ProfilePicture"
+                  src={currentUser.avatar_url}
+                  className="profile"
+                  alt="fotinha"
                 />
-              <div>
-                <h3>Caio Monteiro</h3>
-                <span>@moontieur</span>
-                <p>ReactJS Student</p>
-              </div>
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
               </div>
               <hr />
             </>
           ) : null}
           {repos?.length ? (
-          <div>
-            <h4 className='repositorio'>Repositórios</h4>
-            <List title="Teste1" description="descrição"/>
-            <List title="Teste1" description="descrição"/>
-            <List title="Teste1" description="descrição"/>
+            <div>
+              <h4 className="repositorio">Repositórios</h4>
+            {repos.map(repo => (
+              <List title={repo.name} description={repo.description} />
+            ))}
+            </div>
+          ): null}
+            </div>
           </div>
-          ) : null}
         </div>
-      </div>
-    </div>
-  );
-}
+      );               
+    }
 
 export default App;
+ 
